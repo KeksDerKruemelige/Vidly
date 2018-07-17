929f6417c737433ff5612868d751359012b01840
KeksDerKruemelige <35399719+KeksDerKruemelige@users.noreply.github.com>
2018-07-17 14:24:28 +0200

    Action  Result and Action Parameters

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -9,12 +9,29 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+        // GET: Movies/Random
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
 
             return View(movie);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id = " + id);
+        }
+
+        // GET: Movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
     }
 }
